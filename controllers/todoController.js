90499214dbad4932f8963225033e23017eff8aac
Jcasanova1990 <jeremykcasanova@gmail.com>
2024-01-20 12:11:12 -0500

    created index,create exports

---
+++ controllers/todoController.js
@@ -0,0 +1,40 @@
+const Todo = require('../models/todo.js')
+
+
+// // router.get('/', todoCtrl.index)
+// router.post('/', todoctrl.create)
+// router.put('/:id', todoctrl.update)
+// router.delete('/:id', todoctrl.destroy)
+// // router.get('/:id', todoctrl.show)
+
+
+
+exports.index = async function index (req, res) {
+    try{
+
+    }catch { (error)}
+}
+
+exports.create = async function create (req, res) {
+    try{
+
+    }catch { (error)}
+}
+
+exports.update = async function update(req, res) {
+    try{
+
+    }catch { (error)}
+}
+
+exports.destroy = async function destroy(req, res) {
+    try{
+
+    }catch { (error)}
+}
+
+exports.show = async function show(req, res) {
+    try{
+
+    }catch { (error)}
+}
